0224284ad1e58589db01dbc5b01cc96b68425cc5
Chris Rector <chris@chrisrector.com>
2020-04-26 13:21:06 -0300

    Basic skeleton for app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grapher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
